e2f4dd698da9405171f8c140a3811353104edaba
xyy601chenyan <xyy545688506@gmail.com>
2017-08-02 12:03:22 +0800

    convert review ratings to stars

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
+
   # GET /reviews
   # GET /reviews.json
   def index
@@ -26,10 +28,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.movie_id = @movie.id
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to @movie, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -66,6 +69,10 @@ class ReviewsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :reviews
   devise_for :users
-  resources :movies
+  resources :movies do
+    resources :reviews, except: [:show, :index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "movies#index"
 end
